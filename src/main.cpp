a7a487aabbe619cd9d74fa4185cb14fbfd16c3d9
Fuqiang Huang <36307008+fuqiang07@users.noreply.github.com>
2018-11-26 13:47:14 -0600

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -341,7 +341,7 @@ int main() {
     // Waypoint map to read from
     string map_file_ = "../data/highway_map.csv";
     // The max s value before wrapping around the track back to 0
-    double max_s = 6945.554;
+    const double max_s = 6945.554;
 
     ifstream in_map_(map_file_.c_str(), ifstream::in);
 
@@ -394,8 +394,9 @@ int main() {
     tk::spline path_spline_dy;
     path_spline_dy.set_points(map_waypoints_s, map_waypoints_dy);
 
-    h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
-                                                                                                         uWS::OpCode opCode) {
+    h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy,
+                 &max_s, &path_spline_x, &path_spline_y, &path_spline_dx,
+                 &path_spline_dy](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
         // "42" at the start of the message means there's a websocket message event.
         // The 4 signifies a websocket message
         // The 2 signifies a websocket event
