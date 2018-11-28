e56a8232564c05613ec248131d2132de7dcbaf58
Fuqiang Huang <36307008+fuqiang07@users.noreply.github.com>
2018-11-28 13:12:59 -0600

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,10 @@
 
 #include "map.h"
 #include "global.h"
+#include "predictions.h"
+#include "behavior.h"
+#include "trajectory.h"
+#include "cost.h"
 
 using namespace std;
 
@@ -59,7 +63,8 @@ int main() {
      * **************************************************************** *
      */
 
-    h.onMessage([&map](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
+    h.onMessage([&map, &car, &start, &prev_path_sd](uWS::WebSocket<uWS::SERVER> ws, 
+	char *data, size_t length, uWS::OpCode opCode) {
         // "42" at the start of the message means there's a websocket message event.
         // The 4 signifies a websocket message
         // The 2 signifies a websocket event
